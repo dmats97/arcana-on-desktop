db55cc5b8d48e7b9c9be6d0197c424274211168b
dmats97 <diematsmot@gmail.com>
2019-07-26 16:46:20 +0900

    add function 'draw' and more.

--- main.py
+++ main.py
@@ -21,29 +21,38 @@ class Object:
         return this.level
 
 class Game:
+    Life = [8, 8]
+    CP = [0,0]
+    JokerID = [0, 0]
+    JokerGuage = [0, 0]
+
+    # - objects list
+    new_deck0 = []
+    new_deck1 = []
+    deck0 = []
+    deck1 = []
+    hand0 = []
+    hand1 = []
+    trash0 = []
+    trash1 = []
+    delete0 = []
+    delete1 = []
+    trigger0 = []
+    trigger1 = []
+
+    # - const
+    _Top_Draw = 2
+    _Turn_Seconds = 60
+    _LastTurnNumber = 10
+    _Incremental_JokerGuege_ByLifeDamege = 10
+    _Incremental_JokerGuege_ByTurnEnd = 10
+    _CP_Table = [[2,3,4,5,6,7,7,7,7,7],[3,3,4,5,6,7,7,7,7,7]]
 
     def __init__(this):
         # - initialize
         # pygame.init()
         # window = pygame.display.set_mode((1280,680))
         # pygame.display.set_caption("Pygame Practice")
-
-        this.Life = [8, 8]
-        this.CP = [0,0]
-        this.JokerID = [0, 0]
-        this.JokerGuage = [0, 0]
-
-        # - objects list
-        this.deck0 = []
-        this.deck1 = []
-        this.hand0 = []
-        this.hand1 = []
-        this.trash0 = []
-        this.trash1 = []
-        this.delete0 = []
-        this.delete1 = []
-        this.trigger0 = []
-        this.trigger1 = []
 
         # - load database
         fd = open("data.json")
@@ -54,23 +63,20 @@ class Game:
         for data in csv.reader(fd):
             this.deck0.append(data[0])
             this.deck1.append(data[0])
+            this.new_deck0.append(data[0])
+            this.new_deck1.append(data[0])
 
         for i in this.deck0:
-            print(this.db[str(i)]['name']);
-
-        # - const
-        this._Top_Draw = 2
-        this._Turn_Seconds = 60
-        this._LastTurnNumber = 10
-        this._Incremental_JokerGuege_ByLifeDamege = 10
-        this._Incremental_JokerGuege_ByTurnEnd = 10
-        this._CP_Table = [[2,3,4,5,6,7,7,7,7,7],[3,3,4,5,6,7,7,7,7,7]]
+            print(this.db[str(i)]['name'])
 
     def start(this):
+        random.shuffle(this.deck0)
+        random.shuffle(this.deck1)
+
         while(True):
             a = input('Action:')
             if a=="draw":
-                if draw(0):
+                if this.draw(0):
                     print("あなたはカードを引きました.\n")
                 else:
                     print("エラーが返されました.正常に操作を行えませんでした\n")
@@ -89,8 +95,11 @@ class Game:
                 print("draw\ndrive\n")
 
             if a=="show":
-                #print(this.hand0)
-                print(this.deck0)
+                print("Deck:")
+                print(len(this.deck0))
+                print("\n\nHand:\n")
+                for obj in this.hand0:
+                    print(this.db[str(obj)]['name'])
 
             if a=="exit":
                 break
@@ -108,8 +117,18 @@ class Game:
             #        pygame.quit()
             #        sys.exit()
 
-        def draw(this, player):
+    def draw(this, player):
+        if len(this.hand0)<7:
+            if len(this.deck0)<=0:
+                this.deck0 = this.new_deck0
+            this.hand0.append(this.deck0[0])
+            del this.deck0[0]
+            return 1
+        else:
             return 0
+
+    def drive(this, obj):
+        return 0
 
 newgame = Game()
 newgame.start()
